62513e2705971152d99263d6d9548037d2bb6fe5
github-kamilk <kamil.kowalski51@onet.pl>
2025-04-10 22:33:12 +0200

    E2 dataset

--- utils/prepare_data.py
+++ utils/prepare_data.py
@@ -79,5 +79,64 @@ def prepare_data_e1(dataset, seed=42):
     # Save all data to a single npz file
     np.savez(f'datasets/E1/E1_{dataset}_{seed}_data.npz', **data_dict)
 
-# Example of usage:
-# prepare_data_e1(X, y, 'NSL-KDD', seed=123)
+
+def prepare_data_e2(dataset, seed=42):
+    X, y = get_data(dataset)
+    
+    np.random.seed(seed)
+    
+    # Scenarios definition for each dataset
+    scenarios = {
+        'NSL-KDD': {'normal':5000, 'anomalies':250, 'anomalies_misslabeled':[25,50,125,250], 'test_n':2000, 'test_a':200},
+        'CreditCard': {'normal':5000, 'anomalies':150, 'anomalies_misslabeled':[15,30,75,142], 'test_n':2000, 'test_a':200},
+        'AnnThyroid': {'normal':3000, 'anomalies':150, 'anomalies_misslabeled':[15,30,75,150], 'test_n':1000, 'test_a':100},
+        'EmailSpam-bert': {'normal':2000, 'anomalies':50, 'anomalies_misslabeled':[5,10,25,50], 'test_n':1000, 'test_a':46},
+    }
+    
+    params = scenarios[dataset]
+
+    X_normal = X[y == 0]
+    X_anomalies = X[y == 1]
+
+    # First, create a fixed test set
+    X_n_train, X_n_test = train_test_split(X_normal, test_size=params['test_n'], random_state=seed)
+    X_a_train, X_a_test = train_test_split(X_anomalies, test_size=params['test_a'], random_state=seed)
+
+    X_test = np.vstack((X_n_test, X_a_test))
+    y_test = np.hstack((np.zeros(len(X_n_test)), np.ones(len(X_a_test))))
+
+    # Prepare normal samples once for comparability
+    X_norm_sample = X_n_train[np.random.choice(len(X_n_train), params['normal'], replace=False)]
+
+    # Prepare anomaly indices for incremental inclusion
+    max_anomalies_misslabeled = max(params['anomalies_misslabeled'])
+    anom_indices = np.random.choice(len(X_a_train), max_anomalies_misslabeled+params['anomalies'], replace=False)
+
+    # Prepare each training scenario
+    scenario_labels = ['A', 'B', 'C', 'D']
+
+    data_dict = {'X_test': X_test, 'y_test': y_test}
+
+    for sc_label, anomaly_misslabeled_count in zip(scenario_labels, params['anomalies_misslabeled']):
+        total_anomaly_count = anomaly_misslabeled_count + params['anomalies']
+        X_anom_sample = X_a_train[anom_indices[:total_anomaly_count]]
+        X_train = np.vstack((X_norm_sample, X_anom_sample))
+        y_train = np.hstack((np.zeros(len(X_norm_sample)), np.ones(params['anomalies']), np.zeros(anomaly_misslabeled_count)))
+
+        # Shuffle training set
+        indices = np.arange(len(X_train))
+        np.random.shuffle(indices)
+        X_train, y_train = X_train[indices], y_train[indices]
+
+        # Save datasets into dictionary
+        data_dict[f'X_train_{sc_label}'] = X_train
+        data_dict[f'y_train_{sc_label}'] = y_train
+
+    # Save all data to a single npz file
+    np.savez(f'datasets/E2/E2_{dataset}_{seed}_data.npz', **data_dict)
+
+if __name__ == "__main__":
+    prepare_data_e2('NSL-KDD')
+    prepare_data_e2('CreditCard')
+    prepare_data_e2('AnnThyroid')
+    prepare_data_e2('EmailSpam-bert')    
