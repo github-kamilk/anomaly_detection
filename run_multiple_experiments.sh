6d6ce05b8bb206979b3ada0d39fadd848c585b73
github-kamilk <84456262+github-kamilk@users.noreply.github.com>
2025-04-10 18:13:26 +0200

    Add files via upload

---
+++ run_multiple_experiments.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+num_runs=8
+
+for ((i=1; i<=num_runs; i++))
+do
+  echo "=== RUN $i START ==="
+  python experiment1.py
+  timestamp=$(date +"%Y%m%d_%H%M%S")
+  mv results_e1 "results_e1_run_${i}_${timestamp}"
+  mkdir -p results_e1
+
+  echo "=== RUN $i FINISHED ==="
+done
